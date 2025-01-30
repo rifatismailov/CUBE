ab0943948712e03f3ab7cc1711b7e6a260d6dea4
Rifat Ismailov <refaazer@gmail.com>
2025-01-31 01:21:37 +0200

    CUBE Add SQLiteDatabase Add DatabaseHelper Add MessageManager for save to Database Add ContactManager for save to Database Add FileEncryption for encryption file

--- app/src/main/java/com/example/cube/draw/CircularImageView.java
+++ app/src/main/java/com/example/cube/draw/CircularImageView.java
@@ -11,8 +11,13 @@ import androidx.appcompat.widget.AppCompatImageView;
 
 public class CircularImageView extends AppCompatImageView {
     private Path path;
-    private Paint paint;
+    private Paint imagePaint;
+    private Paint borderPaint;
     private RectF rect;
+
+    // Значення для обвідного кольору та ширини
+    private int borderColor = 0xFF000000; // Чорний колір за замовчуванням
+    private float borderWidth = 4f; // Ширина обвідної лінії
 
     public CircularImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -21,43 +26,52 @@ public class CircularImageView extends AppCompatImageView {
 
     private void init() {
         path = new Path();
-        paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setFilterBitmap(true);
-        paint.setDither(true);
+
+        // Налаштування для зображення
+        imagePaint = new Paint();
+        imagePaint.setAntiAlias(true);
+        imagePaint.setFilterBitmap(true);
+        imagePaint.setDither(true);
+
+        // Налаштування для обвідної лінії
+        borderPaint = new Paint();
+        borderPaint.setAntiAlias(true);
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setColor(borderColor);
+        borderPaint.setStrokeWidth(borderWidth);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        rect = new RectF(0, 0, w, h);
+        rect = new RectF(borderWidth / 2, borderWidth / 2, w - borderWidth / 2, h - borderWidth / 2);
         path.reset();
         path.addOval(rect, Path.Direction.CCW);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        // Враховуємо padding
-        float paddingLeft = getPaddingLeft();
-        float paddingTop = getPaddingTop();
-        float paddingRight = getPaddingRight();
-        float paddingBottom = getPaddingBottom();
-
-        @SuppressLint("DrawAllocation")
-        RectF adjustedRect = new RectF(
-                rect.left + paddingLeft,
-                rect.top + paddingTop,
-                rect.right - paddingRight,
-                rect.bottom - paddingBottom
-        );
-
-        path.reset();
-        path.addOval(adjustedRect, Path.Direction.CCW);
-
+        // Малювання зображення
         canvas.save();
         canvas.clipPath(path);
         super.onDraw(canvas);
         canvas.restore();
+
+        // Малювання обвідної лінії
+        canvas.drawOval(rect, borderPaint);
     }
 
+    // Метод для зміни кольору обвідної лінії
+    public void setBorderColor(int color) {
+        this.borderColor = color;
+        borderPaint.setColor(color);
+        invalidate();
+    }
+
+    // Метод для зміни ширини обвідної лінії
+    public void setBorderWidth(float width) {
+        this.borderWidth = width;
+        borderPaint.setStrokeWidth(width);
+        invalidate();
+    }
 }
