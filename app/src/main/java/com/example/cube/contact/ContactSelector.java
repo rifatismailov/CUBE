5e46a1fd6415dccb40ac55f8ecf5d5ff051324d2
Rifat Ismailov <refaazer@gmail.com>
2025-02-18 14:37:38 +0200

    CUBE Add SQLiteDatabase Add DatabaseHelper Add MessageManager for save to Database Add ContactManager for save to Database Add FileEncryption for encryption file

--- app/src/main/java/com/example/cube/contact/ContactSelector.java
+++ app/src/main/java/com/example/cube/contact/ContactSelector.java
@@ -1,10 +1,21 @@
 package com.example.cube.contact;
 
 public class ContactSelector {
+    private ContactData contactData;
     private String contact;
+
+    public ContactData getContactData() {
+        return contactData;
+    }
 
     public String getContact() {
         return contact;
+    }
+
+    public void setContact(ContactData contactData) {
+        contactData.setSize(0);
+        this.contactData = contactData;
+        this.contact = contactData.getId();
     }
 
     public void setContact(String contact) {
